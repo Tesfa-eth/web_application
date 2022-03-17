068b28136bff40c6cb83aa07f3365073a36e2374
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-03-16 20:42:58 -0400

    Added database model Post

--- RateMySchoolProject/rateMySchoolApp/models.py
+++ RateMySchoolProject/rateMySchoolApp/models.py
@@ -1,3 +1,20 @@
+from ast import mod
 from django.db import models
 
 # Create your models here.
+
+# After adding the models
+# python manage.py makemigrations     : applys the model changes
+# 
+class Post(models.Model):
+    postcontent = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+    user_id = models.IntegerField()
+    postNum = models.IntegerField()
+    upvoteCount = models.IntegerField()
+    downvoteCount = models.IntegerField()
+    ratedBodyID = models.IntegerField()
+    rate_stars = models.IntegerField()
+
+    def __str__(self):
+        return self.postcontent
