d770b1c50b46bec5269ccacff206887332c0d80c
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-03-20 23:37:41 -0400

    added chart using chart.js

--- RateMySchoolProject/RateMySchoolProject/settings.py
+++ RateMySchoolProject/RateMySchoolProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rateMySchoolApp.apps.ratemyschoolappConfig' # might cause some issue
+    'rateMySchoolApp.apps.ratemyschoolappConfig', # might cause some issue
+    'chartjs'
 ]
 
 MIDDLEWARE = [
